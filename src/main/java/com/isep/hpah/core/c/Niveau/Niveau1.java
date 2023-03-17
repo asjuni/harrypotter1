a9ce1730eea4b808876b7dff7cfd483660159221
tp3 <juniortitcheu@gmail.com>
2023-03-17 10:56:47 +0100

    bgh

--- src/main/java/com/isep/hpah/core/c/Niveau/Niveau1.java
+++ src/main/java/com/isep/hpah/core/c/Niveau/Niveau1.java
@@ -1,2 +1,65 @@
-package com.isep.hpah.core.c.Niveau;public class niveau1 {
+package com.isep.hpah.core.c.Niveau;
+
+import java.util.Scanner;
+
+import com.isep.hpah.core.c.Level;
+import lombok.*;
+@Getter@Setter
+
+public class Niveau1 {
+
+    public static int wizardPower;
+
+    public void combattre() {
+        Scanner sc = new Scanner(System.in);
+
+        // initialization of Wizard and troll's characteristics
+        int wizardPower = 100;
+        int trollPower = 10;
+        int wizardLife = wizardPower;
+        // combat loop
+        while (wizardLife > 0 && trollPower > 0) {
+            System.out.println(" \n It's the Wizard's turn.\n" +
+                    " you have three spells to inflict big damage to the troll \n" +
+                    "1. Wingardium Leviosa \n" +
+                    "2. Avada Kedavra  \n" +
+                    "3. Expelliarmus \n" +
+                    "if the spell you cast among these three is one of the least influential, the troll may attack you \n\n");
+            int choice = sc.nextInt();
+
+            // if the Wizard casts the spell Wingardium Leviosa, he wins the combat
+            if (choice == 1) {
+                System.out.println("The Wizard casts the spell Wingardium Leviosa ! \n\n");
+                System.out.println("The troll is lifted in the air and falls heavily to the ground.\n\n");
+                System.out.println("The Wizard wins the combat! \n");
+                trollPower = 0;
+
+            }
+            // otherwise, the troll attacks and inflicts damage to the Wizard
+            else {
+                int damage = (int)(Math.random() * 10) + 1;
+                System.out.println("The troll attacks the Wizard and inflicts " + damage + " points of damage. \n\n");
+                wizardLife -= damage;
+            }
+
+            // if the troll is still alive, it attacks in turn
+            if (trollPower > 0) {
+                int damage = (int)(Math.random() * 6) + 1;
+                System.out.println("It's the troll's turn. It attacks the Wizard and inflicts " + damage + " points of damage.\n\n");
+                wizardLife -= damage;
+            }
+
+            // display of remaining life points
+            System.out.println("Remaining life points:\n");
+            System.out.println("Wizard: " + wizardLife + "\n");
+            System.out.println("Troll: " + trollPower +"\n");
+            System.out.println();
+            System.out.println((char) 27 + "[33m" + "\n\n CONGRATULATIONS YOU HAVE PASSED THIS LEVEL. PRESS ENTER TO PROCEED TO THE NEXT LEVEL \n\n " + (char) 27 + "[0m");
+        }
+
+        // end of combat
+        if (wizardLife <= 0) {
+            System.out.println(" \n\n The troll has defeated the Wizard! \n");
+        }
+    }
 }
--- src/main/java/com/isep/hpah/core/c/Niveau/Niveau2.java
+++ src/main/java/com/isep/hpah/core/c/Niveau/Niveau2.java
@@ -1,2 +1,94 @@
-package com.isep.hpah.core.c.Niveau;public class Niveau2 {
+package com.isep.hpah.core.c.Niveau;
+
+import java.util.Scanner;
+
+import com.isep.hpah.core.c.House;
+import com.isep.hpah.core.c.SortingHat;
+import lombok.*;
+public class Niveau2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean gagne = false;
+// Initialisation de la vie du wizard en utilisant la valeur de la variable du niveau 1
+        int wizardPower = Niveau1.wizardPower;
+
+        // initialisation des caractéristiques du basilic
+        int basilicPower = 12;
+    System.out.println("Bienvenue au combat contre le basilic !");
+            System.out.print("Dans quelle maison êtes-vous ? \n\n");
+
+            // Récupérer le nom de la maison du joueur
+            String maison = sc.nextLine();
+
+            // boucle de combat
+            while (wizardPower > 0 && basilicPower > 0 && !gagne) {
+            System.out.println("C'est le tour du Wizard. \n\n");
+
+            // Si le joueur est un Gryffondor, il doit utiliser l'Épée de Griffondor pour vaincre le basilic
+            if (maison.equals("Gryffondor")) {
+            System.out.println("Vous êtes un Gryffondor. Tapez '1 ou 2 ou 3 ' pour utiliser \n\n" +
+                    "2 .l'Épée de Griffondor \n\n " +
+                    "1. l'Épée de damoclesse \n\n" +
+                    "3 . excalibure \n\n " +
+                    "si vous utiliser la mauvaise arme le basilic vous attaqueras .\n\n");
+
+            int choix = sc.nextInt();
+
+            // si le joueur utilise l'Épée de Griffondor, il gagne le combat
+            if (choix == 2) {
+            System.out.println("Vous utilisez l'Épée de Griffondor !\n\n");
+            System.out.println("Vous réussissez à trancher la tête du basilic.\n\n");
+            System.out.println("Vous avez gagné le combat !\n\n");
+            gagne = true;
+            }
+            // sinon, le basilic attaque et inflige des dégâts au joueur
+            else {
+            int degats = (int)(Math.random() * 5) + 1;
+            System.out.println("Le basilic attaque et vous inflige " + degats + " points de dégâts.\n\n");
+            wizardPower -= degats;
+            }
+                System.out.println("Remaining life points:\n");
+                System.out.println("Wizard: " + wizardPower + "\n");
+                System.out.println("basilic: " + basilicPower +"\n");
+                System.out.println();
+            }
+            // Pour les autres maisons, le joueur doit utiliser Croc du basilic avec Accio pour vaincre le basilic
+            else {
+            System.out.println("Vous êtes de la maison " + maison + ". Tapez '1' pour utiliser Croc du basilic avec Accio.\n\n");
+
+            int choix = sc.nextInt();
+
+            // si le joueur utilise Croc du basilic avec Accio, il gagne le combat
+            if (choix == 1) {
+            System.out.println("Vous utilisez Croc du basilic avec Accio !\n\n");
+            System.out.println("Vous réussissez à transpercer le cœur du basilic.\n\n");
+            System.out.println("Vous avez gagné le combat !\n\n");
+            gagne = true;
+            }
+            // sinon, le basilic attaque et inflige des dégâts au joueur
+            else {
+            int degats = (int)(Math.random() * 5) + 1;
+            System.out.println("Le basilic attaque et vous inflige " + degats + " points de dégâts.\n\n");
+            wizardPower -= degats;
+            }
+                System.out.println("Remaining life points:\n");
+                System.out.println("Wizard: " + wizardPower + "\n");
+                System.out.println("basilic: " + basilicPower +"\n");
+                System.out.println();
+            }
+
+            // Si le basilic est encore en vie, il attaque à son tour
+            if (basilicPower > 0 && !gagne) {
+            int degats = (int)(Math.random() * 4) + 1;
+            System.out.println("C'est le tour du basilic. Il attaque et vous inflige " + degats + " points de dégâts.\n\n");
+            basilicPower -= degats;
+            }
+
+
+            }
+    }
 }
+
+
+// Si le joueur a gagné, afficher
+
