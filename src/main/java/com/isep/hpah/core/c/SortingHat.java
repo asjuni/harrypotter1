43f345a2f7f6ae9119487db45b5e2bdbb3f59cb8
tp3 <juniortitcheu@gmail.com>
2023-03-24 11:59:28 +0100

    isep algorythme et programmation

--- src/main/java/com/isep/hpah/core/c/SortingHat.java
+++ src/main/java/com/isep/hpah/core/c/SortingHat.java
@@ -4,12 +4,12 @@ import static com.isep.hpah.core.c.House.maison;
 @Getter@Setter
 public class SortingHat extends House{
     static {
-        System.out.println(" a class you will be assigned from n " +
-                "among the 4 four by the magic hats n n" +
-                "Gryffindor n" +
-                "Poufsouffle n" +
-                "Serpentard n" +
-                "Serdaigle n");
+        System.out.println("\n=====================\n a class you will be assigned from n " +
+                "among the 4 four by the magic hats \n=====================\n" +
+                "Gryffindor \n" +
+                "Poufsouffle \n" +
+                "Serpentard \n" +
+                "Serdaigle \n=====================\n");
         maison();
 
     }
