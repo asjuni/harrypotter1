772fdbc0bda3a7f7892b8b7174bf4838808e6435
tp3 <juniortitcheu@gmail.com>
2023-03-24 11:58:40 +0100

    isep algorythme et programmation

--- src/main/java/com/isep/hpah/core/c/Niveau/Niveau1.java
+++ src/main/java/com/isep/hpah/core/c/Niveau/Niveau1.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 
 import com.isep.hpah.core.c.Level;
 import lombok.*;
+
+import static com.isep.hpah.core.c.spells.AbstractSpell.clearConsole;
+
 @Getter@Setter
 
-public class Niveau1 {
 
+public class Niveau1 {
 
     public void combattre() {
         Scanner sc = new Scanner(System.in);
@@ -15,49 +18,52 @@ public class Niveau1 {
         // initialization of Wizard and troll's characteristics
         int wizardPower = 100;
         int trollPower = 10;
+
         // combat loop
         while (wizardPower > 0 && trollPower > 0) {
-            System.out.println(" \n It's the Wizard's turn.\n" +
-                    " you have three spells to inflict big damage to the troll \n" +
-                    "1. Wingardium Leviosa \n" +
-                    "2. Avada Kedavra  \n" +
-                    "3. Expelliarmus \n" +
-                    "if the spell you cast among these three is one of the least influential, the troll may attack you \n\n");
+
+            System.out.println("\n=================================================================\nYou have three spells to inflict big damage to the troll:");
+            System.out.println("1. Wingardium Leviosa");
+            System.out.println("2. Avada Kedavra");
+            System.out.println("3. Expelliarmus");
+            System.out.println("If the spell you cast among these three is one of the least influential, the troll may attack you.\n=================================================================\n");
+            System.out.print("Enter your choice: ");
+
             int choice = sc.nextInt();
 
             // if the Wizard casts the spell Wingardium Leviosa, he wins the combat
             if (choice == 1) {
-                System.out.println("The Wizard casts the spell Wingardium Leviosa ! \n\n");
-                System.out.println("The troll is lifted in the air and falls heavily to the ground.\n\n");
-                System.out.println("The Wizard wins the combat! \n");
+                System.out.println("\n=====================\nThe Wizard casts the spell Wingardium Leviosa!\n=====================\n");
+                System.out.println("\n=====================\nThe troll is lifted in the air and falls heavily to the ground.\n=====================\n");
+                System.out.println("\n=====================\nThe Wizard wins the combat!\n=====================\n");
                 trollPower = 0;
-                System.out.println((char) 27 + "[33m" + "\n\n CONGRATULATIONS YOU HAVE PASSED THIS LEVEL. PRESS ENTER TO PROCEED TO THE NEXT LEVEL \n\n " + (char) 27 + "[0m");
-
-            }
-            // otherwise, the troll attacks and inflicts damage to the Wizard
-            else {
-                int damage = (int)(Math.random() * 10) + 1;
-                System.out.println("The troll attacks the Wizard and inflicts " + damage + " points of damage. \n\n");
+                System.out.println("\u001B[33m\n=====================\nCongratulations, you have passed this level.\nPress ENTER to proceed to the next level.\n=====================\n\n\n\u001B[0m");
+                sc.nextLine();
+                sc.nextLine();
+            } else {
+                // otherwise, the troll attacks and inflicts damage to the Wizard
+                int damage = (int) (Math.random() * 10) + 1;
+                System.out.println("\n=====================\nThe troll attacks the Wizard and inflicts " + damage + " points of damage\n=====================\n");
                 wizardPower -= damage;
             }
 
             // if the troll is still alive, it attacks in turn
             if (trollPower > 0) {
-                int damage = (int)(Math.random() * 6) + 1;
-                System.out.println("It's the troll's turn. It attacks the Wizard and inflicts " + damage + " points of damage.\n\n");
+                int damage = (int) (Math.random() * 6) + 1;
+                System.out.println("\n=================================================================\nIt's the troll's turn. It attacks the Wizard and inflicts " + damage + " points of damage.\n=================================================================\n");
                 wizardPower -= damage;
             }
 
             // display of remaining life points
-            System.out.println("Remaining life points:\n");
-            System.out.println("Wizard: " +  wizardPower+ "\n");
-            System.out.println("Troll: " + trollPower +"\n");
-            System.out.println();
+            System.out.println("\n=====================\nRemaining life points:\n=====================\n");
+            System.out.println("\n=====================\nWizard: " + wizardPower );
+            System.out.println("Troll: " + trollPower+ "\n=====================\n");
+            System.out.println("\n");
         }
 
         // end of combat
         if (wizardPower <= 0) {
-            System.out.println(" \n\n The troll has defeated the Wizard! \n");
+            System.out.println(".\n=====================\nThe troll has defeated the Wizard!.\n=====================\n");
         }
     }
 }
