65ceaa8b04bac2f86045a8d63da19353a4bb609f
tp3 <juniortitcheu@gmail.com>
2023-03-24 11:58:10 +0100

    isep algorythme et programmation

--- src/main/java/com/isep/hpah/core/c/House.java
+++ src/main/java/com/isep/hpah/core/c/House.java
@@ -8,16 +8,16 @@ public class House {
     public static void maison() {
         String[] houses = {"Gryffondor", "Poufsouffle", "Serpentard", "Serdaigle"};
         int randomHouseIndex = (int) (Math.random() * houses.length);
-        System.out.println("You have been associated with " + houses[randomHouseIndex] + " \n");
+        System.out.println("\n==============================\nYou have been associated with " + houses[randomHouseIndex] + "\n==============================\n");
         System.out.println(" \uD83C\uDF86 \uD83C\uDF89 \uD83C\uDF8E \uD83C\uDF8A \uD83C\uDF83 \uD83C\uDF87 \n");
         if (houses[randomHouseIndex] == "Gryffondor\n\n") {
-            System.out.println("advantage:" + "Gryffindor sorcerers are more resistant to damage. \n");
+            System.out.println("\n==============================\nadvantage:" + "Gryffindor sorcerers are more resistant to damage. \n==============================\n");
         } else if (houses[randomHouseIndex] == "Poufsouffle\n\n") {
-            System.out.println("advantage:" +"Potions are more effective for Hufflepuff members. \n");
+            System.out.println("\n==============================\nadvantage:" +"Potions are more effective for Hufflepuff members. \n==============================\n");
         } else if (houses[randomHouseIndex] == "Serpentard\n\n") {
-            System.out.println("advantage:" +"Spells do more damage for Slytherin members. \n");
+            System.out.println("\n==============================\nadvantage:" +"Spells do more damage for Slytherin members. \n==============================\n");
         } else {
-            System.out.println("advantage:" +"Ravenclaw wizards are more accurate. \n");
+            System.out.println("\n==============================\nadvantage:" +"Ravenclaw wizards are more accurate.\n==============================\n");
         }
     }
 }
