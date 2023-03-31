597da7e92df9db7384ff9322e4eb81707fbcbb4d
tp3 <juniortitcheu@gmail.com>
2023-03-31 09:13:18 +0200

    isep algorythme et programmation

--- src/main/java/com/isep/hpah/core/c/Niveau/Niveau6.java
+++ src/main/java/com/isep/hpah/core/c/Niveau/Niveau6.java
@@ -12,7 +12,7 @@ public class Niveau6 {
         int mangemotPower = 60;
         boolean defeatedMangemot = false;
 
-        System.out.print("Which Hogwarts House are you in? \n");
+        System.out.print("\n============================\nWhich Hogwarts House are you in? \n============================\n");
         String playerHouse = sc.nextLine();
 
         // Check if the player is a Death Eater or not
@@ -23,34 +23,34 @@ public class Niveau6 {
                 System.out.println("You have joined the Death Eaters. Game over.");
                 return;
             } else {
-                System.out.println("You have chosen not to join the Death Eaters. The battle begins!");
+                System.out.println("\n============================\nYou have chosen not to join the Death Eaters. The battle begins!\n============================\n");
             }
         } else {
-            System.out.println("The battle begins!");
+            System.out.println("\n============================\nThe battle begins!\n============================\n");
         }
 
         while (!defeatedMangemot) {
-            System.out.print("Which spell do you want to cast? ");
+            System.out.print("\n============================\nWhich spell do you want to cast? \n============================\n");
             String spell = sc.nextLine();
 
             if (spell.equalsIgnoreCase("Sectumsempra")) {
                 mangemotPower -= 20;
-                System.out.println("You have successfully eliminated a Death Eater!");
+                System.out.println("\n============================\nYou have successfully eliminated a Death Eater!\n============================\n");
                 if (mangemotPower <= 0) {
-                    System.out.println("Congratulations! You have eliminated all the Death Eaters and saved Hogwarts!");
+                    System.out.println("\n============================\nCongratulations! You have eliminated all the Death Eaters and saved Hogwarts!\n============================\n");
                     System.out.println(".\n=====================\nRemaining life points:\n");
                     System.out.println("Wizard: " + wizardPower);
                     System.out.println("Death Eaters: " + mangemotPower + ".\n=====================\n");
                     defeatedMangemot = true;
                 } else {
-                    System.out.println("There are " + mangemotPower / 20 + " Death Eaters left. Who do you want to target?");
+                    System.out.println("\n============================\nThere are " + mangemotPower / 20 + " Death Eaters left. Who do you want to target?\n============================\n");
                     String target = sc.nextLine();
                     System.out.println("You have targeted " + target + ".");
                 }
             } else if (spell.equalsIgnoreCase("Expelliarmus") || spell.equalsIgnoreCase("Avada Kedavra")) {
-                System.out.println("The spell has no effect on the enemy.");
+                System.out.println("\n============================\nThe spell has no effect on the enemy.\n============================\n");
             } else {
-                System.out.println("The spell is not recognized. Try again!");
+                System.out.println("\n============================\nThe spell is not recognized. Try again!\n============================\n");
             }
         }
         System.out.println("\u001B[33mCongratulations, you have passed this level.Press ENTER to proceed to the next level.\n\n\u001B[0m");
