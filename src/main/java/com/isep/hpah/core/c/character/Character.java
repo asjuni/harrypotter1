67dbe401a4d1dd7a452bf03df4a923056f05afc9
tp3 <juniortitcheu@gmail.com>
2023-03-31 08:35:43 +0200

    isep algorythme et programmation

--- src/main/java/com/isep/hpah/core/c/character/Character.java
+++ src/main/java/com/isep/hpah/core/c/character/Character.java
@@ -4,9 +4,7 @@ import lombok.*;
 public class Character {
     public static void main(String[] args) {
         troll();
-        basilic();
-        detraqueur();
-        peter();
+
     }
     public static void troll() {
     {
@@ -24,51 +22,6 @@ public class Character {
     }
 }
 
-    public static void basilic() {
-        System.out.println("                      /\\/|_");
-        System.out.println("                     / o\\| \\");
-        System.out.println("                    /   \\||_\\");
-        System.out.println("                   /  _  \\\\\\//");
-        System.out.println("                  /  (_)  \\\\/");
-        System.out.println("                 /         \\/");
-        System.out.println("           _____/_         \\_");
-        System.out.println("          /      /__        \\ \\");
-        System.out.println("         |       /  \\        \\ \\");
-        System.out.println("         |      /    \\________\\/");
-        System.out.println("         \\____/      \\_______/");
-    }
-
-    public static void detraqueur() {
-    System.out.println("           ,     ,          ");
-System.out.println("           (\\,;,/)");
-System.out.println("            (o o)");
-System.out.println("    ,----ooO--(_)--Ooo----,");
-System.out.println("   |                       |");
-System.out.println("   |      I am a          |");
-System.out.println("   |     Detraqueur.        |");
-System.out.println("   |                       |");
-System.out.println("   |  ...and you are next. |");
-System.out.println("   |                       |");
-System.out.println("   |    ,;,;____;:;,_      |");
-System.out.println("   |   (      )  '  ' )     |");
-System.out.println("   |    \\____/\\____/      |");
-System.out.println("   |___|   /  \\   |___|");
-System.out.println("  /    \\  /    \\  /    \\");
-System.out.println(" (      \\/      \\/      )");
-System.out.println("  \\                     /");
-System.out.println("   \\___________________/");
-
-}
-public static void peter(){
-
-        System.out.println(" _____     _                 _       \n"
-                + "|_   _|__ | | ___   __ _  __| | ___  \n"
-                + "  | |/ _ \\| |/ _ \\ / _` |/ _` |/ _ \\ \n"
-                + "  | | (_) | | (_) | (_| | (_| | (_) |\n"
-                + "  |_|\\___/|_|\\___/ \\__,_|\\__,_|\\___/ \n"
-                + "                                 ");
-
-}
 
 }
 
